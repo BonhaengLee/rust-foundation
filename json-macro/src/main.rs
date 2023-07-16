e7f6f1483a5f25a64bd397c623fb6b5b9efebb4f
BonhaengLee <coalee22@gmail.com>
2023-07-16 18:18:41 +0900

    json macro 1번 규칙 test

--- json-macro/src/main.rs
+++ json-macro/src/main.rs
@@ -1,8 +1,16 @@
+use rustc_serialize::json::Json;
+
 macro_rules! json {
-  (null) => { Json::Null };
-  ([...]) => { Json::Array(...)}
-  ({...}) => { Json::Object(...)}
-  (???) => { Json::Boolean(...) };
-  (???) => { Json::Number(...) };
-  (???) => { Json::String(...) };
+    (null) => {
+        Json::Null
+    }; // ([...]) => { Json::Array(...)}
+       // ({...}) => { Json::Object(...)}
+       // (???) => { Json::Boolean(...) };
+       // (???) => { Json::Number(...) };
+       // (???) => { Json::String(...) };
+}
+
+#[test]
+fn json_null() {
+    assert_eq!(json!(null), Json::Null);
 }
